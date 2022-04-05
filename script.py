ec2383ac6e6be96a7eaf2e8c23e3f6f9765b498f
tjhasan <tjhasan159@gmail.com>
2022-04-05 16:18:01 -0400

    Added logic for getting retweet and quote status.

--- script.py
+++ script.py
@@ -11,7 +11,9 @@ Todo:
 2) Get Tweet Hashtags
     - Complete.
 3) Likes and Retweets
+    - Complete.
 4) Get Retweet & Quote Status
+    - Complete.
 5) Sarcasm
 '''
 
@@ -42,10 +44,32 @@ def get_Tweet_Hashtags(tweet):
     temp = [i for i in temp if i]
     return temp
 
-def get_Likes_and_Retweets(tweet:str):
+def get_Likes_and_Retweets(tweet):
     retweets = tweet[tweet.rindex('"retweet_count":') + len('"retweet_count":'):tweet.rindex('"favorite_count":')-1:]    
     likes = tweet[tweet.rindex('"favorite_count":') + len('"favorite_count":'):tweet.rindex('"favorited":')-1:]
-    print(retweets, likes)
+    return retweets, likes
+
+def get_Retweet_and_Quote_Status(tweet:str):
+    is_retweet = False
+    is_quote = False
+
+    check_for_RT = tweet[tweet.index('"full_text":"')+len('"full_text":"'):tweet.index(',"truncated":')]
+    if "RT" not in check_for_RT and '"is_quote_status": true,' not in tweet:
+        is_retweet = False
+        is_quote = False
+        return is_retweet, is_quote
+    if "RT" not in check_for_RT and '"is_quote_status": true,' in tweet:
+        is_retweet = False
+        is_quote = True
+        return is_retweet, is_quote
+    if "RT" in check_for_RT and '"is_quote_status": true,' not in tweet:
+        is_retweet = True
+        is_quote = False
+        return is_retweet, is_quote
+    if "RT" in check_for_RT and '"is_quote_status": true,' in tweet:
+        is_retweet = True
+        is_quote = True
+        return is_retweet, is_quote
     pass
 
 path = sys.argv[1]
@@ -57,12 +81,23 @@ for file in files:
     for tweet in all_tweets:
         full_data_for_tweet = []
         
-        # text_result = get_Tweet_Full_Text(tweet)
-        # if text_result is not None:
-        #     full_data_for_tweet.append(text_result)
-        # else:
-        #     # End process for the current tweet. Go to next tweet.
-        #     full_data_for_tweet.append(None)
-        # full_data_for_tweet.append(get_Tweet_Hashtags(tweet))
-        get_Likes_and_Retweets(tweet)
+        text_result = get_Tweet_Full_Text(tweet)
+        if text_result is not None:
+            full_data_for_tweet.append(text_result)
+        else:
+            # End process for the current tweet. Go to next tweet.
+            full_data_for_tweet.append(None)
+        hashtags = get_Tweet_Hashtags(tweet)
+
+        retweets, likes = get_Likes_and_Retweets(tweet)
+
+        is_retweet, is_quote = get_Retweet_and_Quote_Status(tweet)
+
+        full_data_for_tweet.append(hashtags)
+        full_data_for_tweet.append(retweets)
+        full_data_for_tweet.append(likes)
+        full_data_for_tweet.append(is_retweet)
+        full_data_for_tweet.append(is_quote)
+
+        print(full_data_for_tweet)
         quit()
